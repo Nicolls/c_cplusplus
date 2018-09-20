961ef54e5e2fdf9b162d2d9bf7096d0d21b334be
nicolls <851778509@qq.com>
2018-09-20 16:49:18 +0800

    C语言版的学生成绩管理系统

---
+++ c/StudentOS/StudentOS/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StudentOS
+//
+//  Created by Meng,Jiankang on 2018/9/20.
+//  Copyright © 2018年 Meng,Jiankang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
